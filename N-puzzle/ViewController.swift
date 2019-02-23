426f50b246f28d3793a1dba8fc893f69782805b2
ChoadPet <poltavets94vetaliy@gmail.com>
2019-02-23 21:37:00 +0200

    Initial commit

---
+++ N-puzzle/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  N-puzzle
+//
+//  Created by Vetaliy Poltavets on 2/23/19.
+//  Copyright © 2019 unit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
