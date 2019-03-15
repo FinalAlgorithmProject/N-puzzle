bb85bc7d4e3581d225124d93ef7998e55f24fafe
iriska <iriska@inernetbs.net>
2019-03-15 15:44:14 +0200

    Minor Generator update

--- N-puzzle/Services/Generator.swift
+++ N-puzzle/Services/Generator.swift
@@ -42,7 +42,7 @@ enum Arguments: String {
 }
 
 final class Generator {
-
+    
     var solvability: Solvability? = nil
     var iterations = 10000
     var size = 3
@@ -81,7 +81,99 @@ final class Generator {
     func basicInput() {
         print("Please enter parameters: [-s] - \"Forces generation of a solvable puzzle. Overrides -u.\"\n[-u] - \"Forces generation of an unsolvable puzzle\"\n[-i ITERATIONS] - \"Number of passes\"\nsize - \"Size of the puzzle's side. Must be >3.\"\n")
         if let readLine = readLine() {
+            if readLine.contains(Arguments.solvable.rawValue) &&
+                readLine.contains(Arguments.unsolvable.rawValue) {
+                print("Can't be both solvable AND unsolvable, dummy! 🤯")
+                return
+            }
             
+        }
+    }
+    
+    func obtainPuzzleSize(completion: ((_ quit: Bool) -> Void)?) {
+        print("Please enter size of the puzzle's side. Must be > 2.\"\nIf you want to exit, white \"quit\"")
+        while let input = readLine() {
+            guard input != "quit" else {
+                completion?(true)
+                break
+            }
+            
+            if let size = Int(input) {
+                if size > 2 {
+                    self.size = size
+                    completion?(false)
+                } else {
+                    print("Size of the puzzle's side must be > 2.")
+                }
+            } else {
+                print("Please enter correct size of the puzzle's side. It must be integer and > 2.")
+            }
+        }
+    }
+    
+    func obtainPassesCount(completion: ((_ quit: Bool) -> Void)?) {
+        print("Do you want to setup a specific number of iterations? - yes/no")
+        while let input = readLine() {
+            guard input != "quit" else {
+                completion?(true)
+                break
+            }
+            
+            if input == "yes" {
+                print("Please enter number of passes")
+                while let input = readLine() {
+                    guard input != "quit" else {
+                        completion?(true)
+                        break
+                    }
+                    
+                    if let iterations = Int(input) {
+                        if iterations > 0 && iterations <= 10000 {
+                            self.iterations = iterations
+                            completion?(false)
+                        } else {
+                            print("Number of passes must be > 0 and <= 10000.")
+                        }
+                    } else {
+                        print("Please enter correct number of passes. It must be integer and > 0.")
+                    }
+                }
+            } else if input == "no" {
+                completion?(false)
+            } else {
+                print("Please answer 'yes' or 'no'.\"\nIf you want to exit, white \"quit\"")
+            }
+        }
+    }
+    
+    func obtainSolvability(completion: ((_ quit: Bool) -> Void)?) {
+        print("Do you want to set the puzzle solvability? - yes/no")
+        while let input = readLine() {
+            guard input != "quit" else {
+                completion?(true)
+                break
+            }
+            
+            if input == "yes" {
+                print("Please enter parameters: [-s] - Forces generation of a solvable puzzle. Overrides -u.\n[-u] - Forces generation of an unsolvable puzzle")
+                while let input = readLine() {
+                    guard input != "quit" else {
+                        completion?(true)
+                        break
+                    }
+                    
+                    if  iterations = Int(input) {
+                        
+                    } else {
+                        print("Please enter correct number of passes. It must be integer and > 0.")
+                    }
+                }
+            } else if input == "no" {
+                print("Thanks, solvability of the puzzle will be selected randomly.")
+                completion?(false)
+            } else {
+                print("Please answer 'yes' or 'no'.\"\nIf you want to exit, white \"quit\"")
+            }
         }
     }
 }
